f4c836315079c8a89356a2319ad1c5e5d1d85548
WorldDownTown <WorldDownTown@gmail.com>
2017-09-12 10:38:12 +0900

    add empty Xcode proejct.

---
+++ CurvingProgressBarSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurvingProgressBarSample
+//
+//  Created by Keisuke Shoji on 2017/09/12.
+//  Copyright © 2017年 Keisuke Shoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
